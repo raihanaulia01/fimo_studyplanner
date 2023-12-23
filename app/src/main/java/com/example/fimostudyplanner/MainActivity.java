e64f50872a9a0f9e4075836fcb0385fcff28738f
Juwita <primajuwita1212@gmail.com>
2023-12-23 19:55:17 +0700

    udah ada tempat buat ngoding di tiap bagian (home, tasks, flascard, timer, dan profile udah dibuat navbar nya tapi masih ada yang error

--- app/src/main/java/com/example/fimostudyplanner/MainActivity.java
+++ app/src/main/java/com/example/fimostudyplanner/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.fimostudyplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navHome) {
+                    loadFragment(new HomeFragment(), false);
+
+                } else if (itemId == R.id.navTasks) {
+                    loadFragment(new TasksFragment(), false);
+
+                } else if (itemId == R.id.navFlashcards) {
+                    loadFragment(new FlashcardFragment(), false);
+
+                } else if (itemId == R.id.navTimer) {
+                    loadFragment(new TimerFragment(), false);
+
+                } else { // nav profile
+                    loadFragment(new ProfileFragment(),false);
+
+                }
+
+                loadFragment(new HomeFragment(), true);
+
+                return true;
+            }
+
+        });
+
+        loadFragment(new HomeFragment(), true);
+
     }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+
+        fragmentTransaction.commit();
+
+    }
+
 }
+
