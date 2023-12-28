b8cc8a90a43af5cc0e9eb8067e43abd460451d62
Sabrina Dewi <anggraenisabrina12@gmail.com>
2023-12-28 20:38:37 +0700

    all

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,41 @@
-public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+plugins {
+    id("com.android.application")
+}
 
-    // Mendapatkan referensi ke tombol Settings
-    Button settingsButton = rootView.findViewById(R.id.buttonS);
+android {
+    namespace = "com.example.fimostudyplanner"
+    compileSdk = 34
 
-    // Menambahkan onClickListener ke tombol Settings
-    settingsButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            // Membuka SettingsActivity saat tombol "Settings" diklik
-            Intent intent = new Intent(getActivity(), SettingsActivity.class);
-            startActivity(intent);
+    defaultConfig {
+        applicationId = "com.example.fimostudyplanner"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-    });
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
 
-    return rootView;
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.cardview.cardview:1.1.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
 }
